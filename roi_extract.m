fa3aa1966a2a71383e11cb67f78ac066a871a3b6
Linda Geerligs <36542131+lgeerligs@users.noreply.github.com>
2018-08-23 14:45:35 +0200

    Add files via upload

--- roi_extract.m
+++ roi_extract.m
@@ -1,27 +1,22 @@
 function ROI = roi_extract(S);
 
-% ROIfiles  = cell array of cell arrays, ie set of files for each ROI mask
-% Datafiles = cell array of cell arrays, ie set of files for each subject
-% TBA***
-% uROIvals = cell array of vector of values that define ROI for each ROI mask
-
 % Extract ROI data from image volume(s), either 4D (e.g., resting state
 % timecourses) or 3D (e.g., grey matter signal). Gets data from image files
-% themselves; does not require SPM.mat. Will extract data from the image files
-% based on the dimensionality of the ROI data. 
+% themselves; does not require SPM.mat. 
 %
-% Outputs struct ROI with various fields, including 'mean' (ROI mean
-% timecourses over voxels) and 'svd' (First temporal singular value; for 4D 
-% data only).
-% (S.filter (default = 0) = if set to 1, this will filter the voxelwise data prior to averaging or PCA
-% (S.HPC (default = 100) = highpass cut-off (in seconds)
-% (S.LPC (default = 10) = lowpass cut-off (in seconds)
-% S.mask =  binary mask image
-
-% by Rik Henson Feb 2012
-
-%% Parameters:
-
+% ROIfiles  = cell array of cell arrays, ie set of files for each ROI mask
+% Datafiles = cell array of cell arrays, ie set of files for each subject
+% no_svd = 1 means do not do SVD (eg if data large), ie also calculate first eigenvector 
+% output_raw = whether to save all volume x voxel raw data (as well as mean/median/SVD)
+% zero_rel_tol = maximal proportion of voxels with no data (0 var or NaN) in order to include
+% zero_abs_tol = minimum number of voxels needed in order to include
+% uROIvals = cell array of vector of values that define ROI for each ROI mask
+% mask_space = whether to use voxels from mask image (1), or data image (0, default)
+%
+% Original by Rik Henson Feb 2012.
+% Extended by Jason Taylor, 2012.
+% Mask_space option added by Linda Geerligs, May 2016
+% Mask_space option fixed after ">" threshold added, Rik Henson,  July 2017
 
 try ROIfiles = S.ROIfiles;
 catch
@@ -58,33 +53,6 @@ catch
     uROIvals = {};
 end
 
-try filter = S.filter;
-catch
-    filter = 0;
-end
-
-if filter==1
-try HPC = S.HPC; catch 
-    HPC = 1/0.01;
-    warning('Assuming highpass cut-off of %d',HPC); 
-end
-
-try LPC = S.LPC; catch 
-    LPC = 1/0.2;
-    warning('Assuming lowpass cut-off of %d',LPC); 
-end
-
-try TR = S.TR; catch 
-   error('Must pass TR for filtering')
-end
-
-end
-
-try mask = S.mask; mask=spm_vol(mask); usemask=1; catch 
-  usemask=0; warning('No whole brain mask image used'); 
-end
-
-
 if isempty(uROIvals);
     for m=1:length(ROIfiles)
         ROIvals{m} = [];
@@ -105,11 +73,15 @@ else
     end
 end
 
+try mask_space = S.mask_space;
+catch
+    mask_space = 0;
+    warning('Using voxels in data space (not mask space)')
+end
+
 do_overwrite = 0;
 verbose      = 0;
 svd_tol  = 1e-6;
-
-
 
 ROI = struct(); 
 
@@ -120,6 +92,7 @@ for s=1:length(Datafiles)
     VY = spm_vol(fl);
     VY = [VY{:}];
     
+    [~,yXYZmm] = spm_read_vols(VY(1)); % Assume all data in same space!
     % Get inverse transform (assumes all data files are aligned)
     Yinv  = inv(VY(1).mat);
         
@@ -128,20 +101,31 @@ for s=1:length(Datafiles)
         fprintf('\tDoing Mask %d/%d \n',m,length(ROIfiles));
 
         VM = spm_vol(ROIfiles{m});
+        Minv = inv(VM.mat);
+        
         [YM,mXYZmm] = spm_read_vols(VM);
         ROIfstem = spm_str_manip(ROIfiles{m},'rt');
-        
-        if isempty(ROIvals{m})
-            ROIvals{m} = setdiff(unique(YM(:)),0); 
-        elseif strcmp(ROIvals{m},'>0');  % could add more special options here...
-            f = find(YM>0);
-            YM(f) = 1;
-            ROIvals{m} = [1];
-        end
-    
+           
         % Transform ROI XYZ in mm to voxel indices in data:
         yXYZind = Yinv(1:3,1:3)*mXYZmm + repmat(Yinv(1:3,4),1,size(mXYZmm,2));
+        % Transform data XYZ in mm to voxel indices in mask:
+        mXYZind = Minv(1:3,1:3)*yXYZmm + repmat(Minv(1:3,4),1,size(yXYZmm,2));
+        % Transform data XYZ in mm to voxel indices in data:
+        yyXYZind = Yinv(1:3,1:3)*yXYZmm + repmat(Yinv(1:3,4),1,size(yXYZmm,2));
         
+        YMdata = spm_get_data(VM,mXYZind); 
+
+        if isempty(ROIvals{m})
+            ROIvals{m} = setdiff(unique(YM(:)),0); 
+        elseif ROIvals{m}(1) == '>'
+            thr = str2num(ROIvals{m}(2:end));
+            f = find(YM>thr);
+            YM(f) = 1;
+            f = find(YMdata>thr);
+            YMdata(f) = 1;
+            ROIvals{m} = [1];
+        end
+ 
         fprintf('\t\tDoing ROI (/%d):',length(ROIvals{m}));  
         for r=1:length(ROIvals{m})
             fprintf('.%d',r);
@@ -151,15 +135,16 @@ for s=1:length(Datafiles)
             ROI(nr,s).svd_tol  = svd_tol;           
             ROI(nr,s).zero_rel_tol = zero_rel_tol;           
             
-            f = find(YM==ROIvals{m}(r));
-                      
-            d = spm_get_data(VY,yXYZind(:,f)); % Think this is correct!
-            
-            if usemask==1
-            % use only voxels that are part of the mask image.
-            maskdata = spm_get_data(mask,yXYZind(:,f)); 
-            maskdata=repmat(maskdata,[length(VY)  1]);
-            d=d.*maskdata;
+            if mask_space
+                f = find(YM==ROIvals{m}(r));
+                d = spm_get_data(VY,yXYZind(:,f)); 
+                ROI(nr,s).XYZ       = mXYZmm(:,f);
+                ROI(nr,s).XYZcentre = mean(mXYZmm(:,f),2);
+            else
+                f = find(YMdata == ROIvals{m}(r));
+                d = spm_get_data(VY,yyXYZind(:,f));
+                ROI(nr,s).XYZ       = yXYZmm(:,f);
+                ROI(nr,s).XYZcentre = mean(yXYZmm(:,f),2);
             end
             
             Nvox = size(d,2);
@@ -167,36 +152,16 @@ for s=1:length(Datafiles)
                 fprintf('Region %d (%s = %d): %d ',r,ROIfstem,ROIvals{m}(r),length(f));
             end
             ROI(nr,s).ROIval    = ROIvals{m}(r);
-            ROI(nr,s).XYZ       = mXYZmm(:,f);
-            ROI(nr,s).XYZcentre = mean(mXYZmm(:,f),2);
             ROI(nr,s).numvox    = Nvox;
             
-            if filter==1
-                Ns=size(d,1);
-                K   = spm_dctmtx(Ns,Ns);
-                nHP = fix(2*(Ns*TR)/HPC + 1);
-                nLP = fix(2*(Ns*TR)/LPC + 1);
-                K   = K(:,[2:nHP nLP:Ns]);      % Remove initial constant
-                R  = eye(Ns) - K*pinv(K);
-                d = R*d;
-
-                ROI(nr,s).LPC=LPC;
-                ROI(nr,s).HPC=HPC;
-                ROI(nr,s).TR=TR;
-            end
-            
-          
             if output_raw
                 ROI(nr,s).rawdata = d;
             end
             
             % Check for zero-variance voxels:
-            % * this only makes sense for 4D data! 
-            % * remove zeros or NaNs for 3D?
-            zero_vox = (var(d)==0);
+            zero_vox = (var(d)==0 | isnan(var(d)));
             zero_count = sum(zero_vox); 
             ROI(nr,s).nonzerovox = Nvox-zero_count;
-
             
             if (zero_count/Nvox > zero_rel_tol) | ((Nvox - zero_count) < zero_abs_tol)
                 %if verbose
@@ -210,7 +175,12 @@ for s=1:length(Datafiles)
             else
                 % Remove zero-variance voxels:
                 f = setdiff(f,zero_vox);
-                d = spm_get_data(VY,yXYZind(:,f));
+               
+                if mask_space
+                    d = spm_get_data(VY,yXYZind(:,f)); 
+                else
+                    d = spm_get_data(VY,yyXYZind(:,f));
+                end
                 
                 if verbose
                     fprintf('(%d nonzero) voxels\n',Nvox-zero_count);
@@ -218,10 +188,9 @@ for s=1:length(Datafiles)
                 
                 % Remove voxel-wise mean if data from several volumes(?)
 %                 if numel(d)>max(size(d))
-%                     dd = d-repmat(mean(d,1),size(d,1),1);
+%                     d = d-repmat(mean(d,1),size(d,1),1);
 %                 end
                 
-                            
                 % MEAN/MEDIAN:
                 ROI(nr,s).mean   = mean(d,2);
                 ROI(nr,s).median = median(d,2);
@@ -258,73 +227,3 @@ for s=1:length(Datafiles)
         fprintf('\n')
     end
 end
-
-%         % -- aa: should do output by stream --
-%         save(sprintf('ROI_%s.mat',ROIfstem),'ROI');
-%         
-        %==========================================================
-        % PLOT
-
-%         % -- aa: leave this out (?) --
-%         
-%         % Get data:
-%         data = [ROI.mean];
-%         failed = isnan(sum(data,1));
-%         
-%         % Plot ROI centres:
-%         xyz = [ROI.XYZcentre];
-%         figure(1); clf;
-%         subplot(3,1,1);
-%         for i=1:size(xyz,2)
-%             if any(isnan(ROI(i).mean))
-%                 plot3(xyz(1,i),xyz(2,i),xyz(3,i),'rx'); hold on;
-%             elseif any(isnan(ROI(i).svd_pvar))
-%                 plot3(xyz(1,i),xyz(2,i),xyz(3,i),'go'); hold on;
-%             else
-%                 plot3(xyz(1,i),xyz(2,i),xyz(3,i),'b.'); hold on;
-%             end
-%         end
-%         axis equal
-%         view([90 0])
-%         title(sprintf('%s (%d ROIs)',cbuid,500-sum(failed)));
-%         axis off
-%         
-%         % Plot MEANS:
-%         data = [ROI.mean];
-%         failed = isnan(sum(data,1));
-%         data = data(:,~failed);
-%         subplot(3,2,3);
-%         imagesc(data);
-%         axis square
-%         title('Means'); ylabel('Time'); xlabel('ROI');
-%         
-%         % Get covariance:
-%         dcov = cov(data);
-%         subplot(3,2,5);
-%         imagesc(dcov);
-%         axis square
-%         title('Cov(Means)'); ylabel('ROI'); xlabel('ROI');
-%         
-%         print(1,'-dpng','Figure_ROI_summary.png');
-%         
-%         % Plot SVD:
-%         data = [ROI.svd];
-%         failed = isnan(sum(data,1));
-%         data = data(:,~failed);
-%         subplot(3,2,4);
-%         imagesc(data);
-%         axis square
-%         title('SVDs'); ylabel('Time'); xlabel('ROI');
-%         
-%         % Get covariance:
-%         dcov = cov(data);
-%         subplot(3,2,6);
-%         imagesc(dcov);
-%         axis square
-%         title('Cov(SVDs)'); ylabel('ROI'); xlabel('ROI');
-%         
-%         print(1,'-dpng','Figure_ROI_summary.png');
-%         
-%         %==========================================================
-
-%fprintf('\n-+- DONE -+-\n');
